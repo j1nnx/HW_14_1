ebd4eee231363e8bfb066f95a1c4602b3942022b
j1nnx <neykerez@gmail.com>
2024-09-25 18:35:51 +0300

    lol 13

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
@@ -6,18 +9,25 @@ class Product:
         self.quantity = quantity
 
     @property
-    def price(self):
+    def price(self) -> Any:
         return self.__price
 
-    def price_(self, value: float):
+    def price_(self, value: float) -> Any:
         if value <= 0:
-            return 'Цена не должна быть нулевая или отрицательная'
+            return "Цена не должна быть нулевая или отрицательная"
         else:
             self.__price = value
 
     @classmethod
-    def new_product(cls, product_info: dict):
-        return cls(product_info['name'], product_info['description'], product_info['price'], product_info['quantity'])
+    def new_product(cls, product_info: dict) -> Any:
+        return cls(product_info["name"], product_info["description"], product_info["price"], product_info["quantity"])
+
+    def __str__(self) -> Any:
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if isinstance(other, Product):
+            return (self.price * self.quantity) + (other.price * other.quantity)
 
 
 class Category:
@@ -32,16 +42,19 @@ class Category:
 
         Category.category_count += 1
 
-    def add_product(self, product: Product):
+    def add_product(self, product: Product) -> Any:
         if isinstance(product, Product):
             self.__products.append(product)
             Category.product_count += 1
         else:
-            raise ValueError('Что-то не работает')
+            raise ValueError("Что-то не работает")
 
     @property
-    def products(self):
+    def products(self) -> Any:
         return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
+
+    def __str__(self) -> Any:
+        return f"{self.name}, количество продуктов: {Category.product_count} шт."
 
 
 if __name__ == "__main__":
@@ -49,31 +62,20 @@ if __name__ == "__main__":
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни")
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
 
-    category1.add_product(product1)
-    category1.add_product(product2)
-    category1.add_product(product3)
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(str(category1))
 
     print(category1.products)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(Category.product_count)
-
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5})
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.__price = 800
-    print(new_product.price)
-    new_product.__price = -100
-    print(new_product.price)
-    new_product.__price = 0
-    print(new_product.price)
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,51 +1,41 @@
 from typing import Any
 
-import pytest
-
 from src.main import Category, Product
 
-
-@pytest.fixture()
-def test_product() -> Any:
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-
-
-def test_product_init(test_product: Any) -> Any:
-    assert test_product.name == "Samsung Galaxy S23 Ultra"
-    assert test_product.description == "256GB, Серый цвет, 200MP камера"
-    assert test_product.price == 180000.0
-    assert test_product.quantity == 5
+product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
 
-@pytest.fixture()
-def test_category() -> Any:
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    category2 = Category(
-        "Телевизоры",
-        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-    )
-    return category2
+def test_str_product() -> Any:
+    assert str(product1) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    assert str(product2) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+    assert str(product3) == "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
 
 
-def test_category_init(test_category: Any) -> Any:
-    assert test_category.name == "Телевизоры"
-    assert (
-        test_category.description
-        == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
-    )
+category1 = Category(
+    "Смартфоны",
+    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+    [product1, product2, product3],
+)
 
 
-@pytest.fixture()
-def test_new_product():
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5})
-    return new_product
+def test_str_category() -> Any:
+    assert str(category1) == "Смартфоны, количество продуктов: 0 шт."
 
 
-def test_new_product_init(test_new_product):
-    assert test_new_product.name == "Samsung Galaxy S23 Ultra"
-    assert test_new_product.description == "256GB, Серый цвет, 200MP камера"
-    assert test_new_product.price == 180000.0
-    assert test_new_product.quantity == 5
+expected_products = [
+    "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.",
+    "Iphone 15, 210000.0 руб. Остаток: 8 шт.",
+    "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.",
+]
 
+
+def test_method_category() -> Any:
+    assert category1.products == expected_products
+
+
+def test_add_init() -> Any:
+    assert (product1 + product2) == (180000.0 * 5 + 210000.0 * 8)
+    assert (product1 + product3) == (180000.0 * 5 + 31000.0 * 14)
+    assert (product2 + product3) == (210000.0 * 8 + 31000.0 * 14)
