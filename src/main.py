519f2eca80b2dd3b82807090ab9a46ae5b5e1aa3
j1nnx <neykerez@gmail.com>
2024-10-04 14:57:28 +0300

    lol 16

--- src/main.py
+++ src/main.py
@@ -1,8 +1,33 @@
 from typing import Any
+from abc import ABC, abstractmethod
 
 
-class Product:
+class BaseProduct(ABC):
     def __init__(self, name: str, description: str, price: float, quantity: int):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+    @property
+    @abstractmethod
+    def price(self) -> Any:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> Any:
+        pass
+
+
+class MixinLog:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(f"Класс {self.__class__.__name__} с параметрами: {args}, {kwargs}")
+
+
+class Product(MixinLog, BaseProduct):
+    def __init__(self, name: str, description: str, price: float, quantity: int, *args, **kwargs):
+        super().__init__(name, description, price, quantity)
         self.name = name
         self.description = description
         self.__price = price
@@ -78,39 +103,44 @@ class Category:
 
 
 # if __name__ == '__main__':
-#     smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
-#                              "S23 Ultra", 256, "Серый")
-#     smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
-#     smartphone3 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 #
-#     grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
-#     grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+#     print(product1.name)
+#     print(product1.description)
+#     print(product1.price)
+#     print(product1.quantity)
 #
-#     smartphone_sum = smartphone1 + smartphone2
-#     print(smartphone_sum)
+#     print(product2.name)
+#     print(product2.description)
+#     print(product2.price)
+#     print(product2.quantity)
 #
-#     grass_sum = grass1 + grass2
-#     print(grass_sum)
+#     print(product3.name)
+#     print(product3.description)
+#     print(product3.price)
+#     print(product3.quantity)
 #
-#     try:
-#         invalid_sum = smartphone1 + grass1
-#     except TypeError:
-#         print("Возникла ошибка TypeError при попытке сложения")
-#     else:
-#         print("Не возникла ошибка TypeError при попытке сложения")
+#     category1 = Category("Смартфоны",
+#                          "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#                          [product1, product2, product3])
 #
-#     category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
-#     category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
+#     print(category1.name == "Смартфоны")
+#     print(category1.description)
+#     print(len(category1.products))
+#     print(category1.category_count)
+#     print(category1.product_count)
 #
-#     category_smartphones.add_product(smartphone3)
+#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#     category2 = Category("Телевизоры",
+#                          "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+#                          [product4])
 #
-#     print(category_smartphones.products)
+#     print(category2.name)
+#     print(category2.description)
+#     print(len(category2.products))
+#     print(category2.products)
 #
+#     print(Category.category_count)
 #     print(Category.product_count)
-#
-#     try:
-#         category_smartphones.add_product("Not a product")
-#     except TypeError:
-#         print("Возникла ошибка TypeError при добавлении не продукта")
-#     else:
-#         print("Не возникла ошибка TypeError при добавлении не продукта")
